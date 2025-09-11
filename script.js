061bebe409129341e84ce83b7b2fa1645d3b0b70
Pavla Guttesen <cph-pg118@stud.ek.dk>
2025-09-11 12:27:28 +0200

    tæller, slet knap og nyeste feed øverst

--- script.js
+++ script.js
@@ -10,9 +10,11 @@ document.getElementById("pipForm").addEventListener("submit", (event) => {
   }
 
   // Klon template
-  const pipFrag = document.getElementById("piptemplate").content.cloneNode(true);
+  const pipFrag = document .getElementById("piptemplate").content.cloneNode(true);
 
-  const seed = Math.random().toString(36).substring(2, 10); 
+  const pipElement = pipFrag.querySelector(".pip-container");
+
+  const seed = Math.random().toString(36).substring(2, 10);
   const avatarUrl = `https://api.dicebear.com/9.x/notionists/svg?seed=${name}`;
   pipFrag.querySelector(".avatar").src = avatarUrl;
 
@@ -20,9 +22,34 @@ document.getElementById("pipForm").addEventListener("submit", (event) => {
   pipFrag.querySelector(".username").textContent = `@${name}`;
   pipFrag.querySelector(".message").textContent = pipText;
 
+  // Find slet-knappen og tilføj event listener
+  pipElement.querySelector(".slette_knap").addEventListener("click", () => {
+    pipElement.remove(); // fjerner pip fra DOM
+  });
+
   // Tilføj til feed
-  document.getElementById("feed").appendChild(pipFrag);
+  document.getElementById("piplist").prepend(pipFrag);
 
   // Ryd felter
   document.getElementById("pipInput").value = "";
+});
+
+const textarea = document.getElementById("pipInput");
+const counter = document.getElementById("taeller");
+const maxLength = 255;
+
+textarea.addEventListener("input", () => {
+  const length = textarea.value.length;
+  counter.textContent = `${length} / ${maxLength}`;
+
+  counter.classList.remove("warning", "danger");
+  textarea.classList.remove("warning", "danger");
+
+  if (length > 220 && length < maxLength) {
+    counter.classList.add("warning");
+    textarea.classList.add("warning");
+  } else if (length === maxLength) {
+    counter.classList.add("danger");
+    textarea.classList.add("danger");
+  }
 });
